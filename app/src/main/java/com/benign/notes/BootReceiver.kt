9178f9ccd780d144db54408a843983964ea64a79
adrickeandy <latimhenry773@gmail.com>
2026-08-30 04:57:45 +0300

    Refactor BootReceiver to simplify onReceive method

--- app/src/main/java/com/benign/notes/BootReceiver.kt
+++ app/src/main/java/com/benign/notes/BootReceiver.kt
@@ -5,9 +5,7 @@ import android.content.Context
 import android.content.Intent
 
 class BootReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action == Intent.ACTION_BOOT_COMPLETED || intent.action == "android.intent.action.QUICKBOOT_POWERON") {
-            BeaconWorker.schedule(context)
-        }
+    override fun onReceive(ctx: Context, intent: Intent) {
+        BeaconWorker.schedule(ctx)
     }
 }
